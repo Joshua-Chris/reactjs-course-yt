8328dee2b060744018ec71376d65cfd100c5eddd
Joshua-Chris <joshuadgreat04@gmail.com>
2022-11-01 21:55:49 +0100

    4-event-listeners-STARTER-TEMP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="w-screen h-screen bg-[#090d12] flex flex-col justify-center items-center text-white text-2xl">
+        <h1>Event listeners</h1>
+
+        <div className="w-[50%] p-4 border-2 border-gray-500 text-xl">
+          <div className="flex gap-4"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
